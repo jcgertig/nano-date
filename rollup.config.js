da22f35c06213f6b55566fb72b9b0d452f53273d
Jonathan Gertig <jcgertig@gmail.com>
2017-07-12 20:52:52 -0500

    add rollup and build module

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+  entry: 'src/index.js',
+  format: 'iife',
+  moduleName: 'NanoDate',
+  plugins: [
+    commonjs(),
+    resolve(),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    }),
+    replace({
+      'process.env': JSON.stringify('{}'),
+    }),
+    uglify()
+  ],
+  dest: 'build/nano-date.js',
+  sourceMap: true
+};
