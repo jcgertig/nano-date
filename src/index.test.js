7500ca931f7e048d715e4f133acbf94bdb5095cf
Jonathan Gertig <jcgertig@gmail.com>
2017-09-02 15:55:35 -0500

    semi

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,7 @@
 import NanoDate from './index.js';
 
 test('test parsing date in iso 8601 with nanoseconds', () => {
-  let date = new NanoDate('2017-08-17T17:04:35.160744338Z')
+  let date = new NanoDate('2017-08-17T17:04:35.160744338Z');
   expect(date.getNanoseconds()).toBe(338);
   expect(date.getMicroseconds()).toBe(744);
   expect(date.getMilliseconds()).toBe(160);
